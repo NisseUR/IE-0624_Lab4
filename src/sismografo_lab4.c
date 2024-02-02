1d5fda87d6090687657dd168cc0ce074cac9bc1e
Alonso J Jiménez A <alonso.jimenezanchia@ucr.ac.cr>
2024-02-02 13:51:48 -0600

    main function

--- src/sismografo_lab4.c
+++ src/sismografo_lab4.c
@@ -29,3 +29,34 @@
 #include "usart.h"
 
 
+// Librerias para el MCU STM32F4
+
+/* DECLARACION FUNCIONES */
+void inicializar_sistema();
+
+
+/* FUNCIONES */
+          
+void inicializar_sistema(){
+    /* Funcion para inicializar el sistema 
+    basado en ejemplo lcd-serial.c*/
+    clock_setup();
+	console_setup(115200);
+	sdram_init();
+	lcd_spi_init();
+    gfx_init(lcd_draw_pixel, 240, 320);
+    gfx_fillScreen(LCD_GREY);
+}
+
+
+int main(void){
+
+    inicializar_sistema();
+
+    while(1){
+        gfx_puts("HOLA MUNDO!");
+        gfx_fillScreen(LCD_RED);
+        gfx_fillScreen(LCD_WHITE);
+        gfx_fillScreen(LCD_BLUE);
+    }
+}
