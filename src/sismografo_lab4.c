b0f94b5e2ab7c990a22f17b541111fad199ceda7
nisse2112 <denisse.ugalde@ucr.ac.cr>
2024-02-02 13:24:06 -0600

    se agregan bibliotecas estandares

---
+++ src/sismografo_lab4.c
@@ -0,0 +1,15 @@
+/*
+ * Laboratorio #4 STM32: GPIO, ADC, comunicaciones, Iot
+ *
+ * Estudiantes: Denisse Ugalde Rivera y Alonso José Jiménez Anchía 
+ * 
+ */
+
+// Bibliotecas
+
+// Bibliotecas estandares
+#include <stdio.h>  // Para operaciones de entrada y salida básicas.
+#include <stdint.h> // Define tipos de datos de ancho fijo.
+#include <stdlib.h> // Para operaciones generales como conversión de números a cadenas.
+#include <string.h> // Para manipular arrays de caracteres y strings.
+#include <math.h>   // Para realizar cálculos matemáticos complejos.
