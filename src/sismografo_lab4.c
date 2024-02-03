e851123958f23f1654d4409f67a07d112445c6f3
nisse2112 <denisse.ugalde@ucr.ac.cr>
2024-02-03 13:41:45 -0600

    se cambia el encabezado

--- src/sismografo_lab4.c
+++ src/sismografo_lab4.c
@@ -1,24 +1,17 @@
 /*
- * This file is part of the libopencm3 project.
+ * Laboratorio #4 STM32: GPIO, ADC, comunicaciones, Iot
  *
- * Copyright (C) 2014 Chuck McManis <cmcmanis@mcmanis.com>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * Estudiantes: Denisse Ugalde Rivera y Alonso José Jiménez Anchía 
+ * 
  */
+// Bibliotecas
 
-#include <stdint.h>
-#include <math.h>
+// Bibliotecas estandares
+
+#include <stdint.h> // Define tipos de datos de ancho fijo.
+#include <math.h> // Para realizar cálculos matemáticos complejos.
+
+// Bibliotecas obtenidas de la carpeta example
 #include "clock.h"
 #include "console.h"
 #include "sdram.h"
