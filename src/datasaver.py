a622989bd17454edee759ca28ff78fed11afc8a6
Alonso J Jiménez A <alonso.jimenezanchia@ucr.ac.cr>
2024-02-06 10:17:24 -0600

    Actualizacion datasaver.py

--- src/datasaver.py
+++ src/datasaver.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import serial
 import paho.mqtt.client as mqtt
 import json
@@ -6,27 +8,33 @@ import time
 # Conexión MQTT
 broker_address = "iot.eie.ucr.ac.cr"
 mqtt_port = 1883
+#mqtt_port = 8080
 mqtt_topic = "v1/devices/me/telemetry"
-mqtt_username = "STM32_User"
-mqtt_password = "3r7m5422aw3lw0vk32su"
+mqtt_username = "Lab4_C07893_B63561"
+mqtt_password = "yq7cgc21emlixma2q1p0"
 
 # Se configura el puerto serie
 serial_port = "/dev/ttyACM0"  
 baudrate = 115200  # Velocidad de baudios
-
 
 # Crear instancia del cliente MQTT
 mqtt_client = mqtt.Client("STM32_Serial_MQTT")
 
 # Definir los manejadores de eventos MQTT
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    if rc == 0:
+        print("Connected to MQTT broker")
+    else:
+        print(f"Failed to connect to MQTT broker with result code {rc}")
+    #print("Connected with result code " + str(rc))
 
 def on_disconnect(client, userdata, rc):
-    print("Disconnected from MQTT broker")
+    #print("Disconnected from MQTT broker")
+    print(f"Disconnected from MQTT broker with result code {rc}")
 
 # Funciones de callback
 mqtt_client.on_connect = on_connect
+
 mqtt_client.on_disconnect = on_disconnect
 
 # Nombre de usuario y contraseña para MQTT
